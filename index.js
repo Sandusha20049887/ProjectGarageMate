3d5309fdb1e604cf0bf6f5690a990484e29cd084
Sandusha20049887 <sandusha1997@gmail.com>
2024-10-28 21:16:46 +0000

    Added sample data to mongodb and wrote a get method

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app=express()
+const port=8080
+const mongo_url='mongodb+srv://sa:sa@projectdb.enabx.mongodb.net/?retryWrites=true&w=majority&appName=projectdb'
+
+app.use(bodyParser.json());
+
+//Database connection
+mongoose.connect(mongo_url).then(() => {
+  console.log("database connected!");
+}) 
+.catch((error) => console.log("errror - "+error));
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  email: String
+});
+
+const User = mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => {
-  res.send('Hello World! master')
+
+  User.find()
+      .then(user => res.send(user))
+      .catch(err => res.send('Error: ' +  err));
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
+
